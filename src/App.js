e3954ad7ea2e949de377f36580158ab5916e47ff
Susen Jayswal <105514429+Susenjayswal@users.noreply.github.com>
2024-06-17 11:40:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-
+import './Appa.css'
+import Bodypart from './components/Bodypart';
+import Navbar from './components/Navbar';
+import Register from './components/Register';
+import RegisterStudent from './components/RegisterStudent';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+    <Navbar title="Solo Scholar"/>
+    <Bodypart/>
+    <Register/>
+    <RegisterStudent/>  
+   </>
   );
 }
-
 export default App;
