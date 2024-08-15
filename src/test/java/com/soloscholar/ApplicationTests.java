3a0fca1c24d74bcc96aac7f1cf98fcc5cdbd6d24
Susenjayswal <susenjayswal@gmail.com>
2024-08-15 21:18:27 +0530

    update readme

---
+++ src/test/java/com/soloscholar/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soloscholar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
